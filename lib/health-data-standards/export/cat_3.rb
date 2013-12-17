abc04995ae479cb4bb8b2521f508b27cdce44e26
David Rom <david.rom@kareo.com>
2013-12-17 11:29:08 -0800

    Fixes from testing

--- lib/health-data-standards/export/cat_3.rb
+++ lib/health-data-standards/export/cat_3.rb
@@ -10,21 +10,20 @@ module HealthDataStandards
       end
 
       def export(measures, header, effective_date, start_date, end_date, filter=nil,test_id=nil)
-        results = {}
-        
         qc = HealthDataStandards::CQM::QueryCache
-        
-        collected_measures = measures.inject({}) {|agg, measure| (agg[measure.hqmf_id] ||= []) << measure }
-        results = (collected_measures.map do |hqmf_id, measures|
-          # Any stratified measures means everything should be aggregated
-          if measures.any?(&:is_stratified?)
-            aggregates = [qc.aggregate_measure(measure.hqmf_id, effective_date, filter, test_id)]
-          else
-            aggregates = measures.map {|m| qc.aggregate_measure(m.hqmf_id, effective_date, filter, test_id, m.sub_id)}
-          end
-          
-          [hqmf_id, aggregates]
-        end).to_hash
+
+        results = Hash[
+            collected_measures.map do |hqmf_id, measures|
+              # Any stratified measures means everything should be aggregated
+              if measures.any?(&:is_stratified?)
+                aggregates = [qc.aggregate_measure(hqmf_id, effective_date, filter, test_id)]
+              else
+                aggregates = measures.map {|m| qc.aggregate_measure(hqmf_id, effective_date, filter, test_id, m.sub_id)}
+              end
+
+              [hqmf_id, aggregates]
+            end
+        ]
         
         @rendering_context.render(:template => 'show', 
                                   :locals => {:measures => measures, :start_date => start_date, 
