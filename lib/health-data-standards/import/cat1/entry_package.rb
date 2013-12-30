c3d50966681b53330c2acda729e84bbee0d27d9f
David Rom <david.rom@kareo.com>
2013-12-30 12:10:06 -0800

    Attempt to normalize entry OIDs against their negation status

--- lib/health-data-standards/import/cat1/entry_package.rb
+++ lib/health-data-standards/import/cat1/entry_package.rb
@@ -13,8 +13,27 @@ module HealthDataStandards
 
         def package_entries (doc, nrh)
           entries = self.importer_type.create_entries(doc, nrh)
-          entries.each do |entry| 
-            entry.oid = self.hqmf_oid
+
+          # Figure out correct negative/positive oids for this importer
+          oid_mapping = HQMFTemplateHelper.template_id_map[hqmf_oid]
+          if oid_mapping['negation']
+            negative_oid= hqmf_oid
+            positive_oid = HQMFTemplateHelper.template_id_by_definition_and_status(
+                oid_mapping['definition'], oid_mapping['status'], true)
+          else
+            positive_oid = hqmf_oid
+            negative_oid = HQMFTemplateHelper.template_id_by_definition_and_status(
+                oid_mapping['definition'], oid_mapping['status'], false)
+          end
+
+          entries.each do |entry|
+            # Customize oid for this specific entry
+            if !!entry.negationInd
+              entry.oid = negative_oid
+            else
+              entry.oid = positive_oid
+            end
+
             entry.status = self.status
           end          
           entries
