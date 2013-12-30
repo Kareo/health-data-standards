3c143c46afd43d6d2ad0839bda4e7f0d83ce89f6
David Rom <david.rom@kareo.com>
2013-12-30 14:01:39 -0800

    Fix for cat1 exports not supporting both negated and non-negated entries
    Squashed commit of the following:
    
    commit 72906dcbcc9eb62c4281189d1400e0105b6d5070
    Author: David Rom <david.rom@kareo.com>
    Date:   Mon Dec 30 13:49:55 2013 -0800
    
        Fix inversed third param
    
    commit 8cb0d55b76242f1cffbe24e3cbc0fdaeb30bec49
    Author: David Rom <david.rom@kareo.com>
    Date:   Mon Dec 30 12:12:25 2013 -0800
    
        Fully qualify class to fix ambiguity error
    
    commit c3d50966681b53330c2acda729e84bbee0d27d9f
    Author: David Rom <david.rom@kareo.com>
    Date:   Mon Dec 30 12:10:06 2013 -0800
    
        Attempt to normalize entry OIDs against their negation status

--- lib/health-data-standards/import/cat1/entry_package.rb
+++ lib/health-data-standards/import/cat1/entry_package.rb
@@ -13,8 +13,27 @@ module HealthDataStandards
 
         def package_entries (doc, nrh)
           entries = self.importer_type.create_entries(doc, nrh)
-          entries.each do |entry| 
-            entry.oid = self.hqmf_oid
+
+          # Figure out correct negative/positive oids for this importer
+          oid_mapping = HealthDataStandards::Util::HQMFTemplateHelper.template_id_map[hqmf_oid]
+          if oid_mapping['negation']
+            negative_oid= hqmf_oid
+            positive_oid = HealthDataStandards::Util::HQMFTemplateHelper.template_id_by_definition_and_status(
+                oid_mapping['definition'], oid_mapping['status'], false)
+          else
+            positive_oid = hqmf_oid
+            negative_oid = HealthDataStandards::Util::HQMFTemplateHelper.template_id_by_definition_and_status(
+                oid_mapping['definition'], oid_mapping['status'], true)
+          end
+
+          entries.each do |entry|
+            # Customize oid for this specific entry
+            if !!entry.negationInd
+              entry.oid = negative_oid
+            else
+              entry.oid = positive_oid
+            end
+
             entry.status = self.status
           end          
           entries
