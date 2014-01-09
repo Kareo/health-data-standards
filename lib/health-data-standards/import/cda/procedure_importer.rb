5032b4d240edbecfa9358d0ea41d2e216cf38b50
Jackie Yeung <jackie.yeung@kareo.com>
2014-01-08 18:44:53 -0800

    updating procedure importer to work with CMS 69 (BMI follow up)

--- lib/health-data-standards/import/cda/procedure_importer.rb
+++ lib/health-data-standards/import/cda/procedure_importer.rb
@@ -8,6 +8,7 @@ module HealthDataStandards
         def initialize(entry_finder=EntryFinder.new("//cda:section[cda:templateId/@root!='2.16.840.1.113883.3.88.11.83.124']//cda:procedure"))
           super(entry_finder)
           @entry_class = Procedure
+          @reason_xpath = "./cda:entryRelationship[@typeCode='RSON']/cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.24.3.88']/cda:value"
           
           # The "Preventive Care and Screening: Screening for Clinical Depression and Follow-Up Plan" measure was failing
           # because this was not getting on procedure entries, letting it back in!
@@ -19,6 +20,8 @@ module HealthDataStandards
           extract_performer(entry_element, procedure)
           extract_site(entry_element, procedure)
           extract_negation(entry_element, procedure)
+          procedure.reason = extract_code(entry_element, @reason_xpath, 'SNOMED-CT')
+          procedure.status_code = {'HL7 ActStatus' => ['ordered']}
           procedure
         end
 
@@ -31,6 +34,18 @@ module HealthDataStandards
 
         def extract_site(parent_element, procedure)
           procedure.site = extract_code(parent_element, "./cda:targetSiteCode")
+        end
+
+        def extract_code(parent_element, code_xpath, code_system=nil)
+          code_element = parent_element.at_xpath(code_xpath)
+          code_hash = nil
+          if code_element
+            code_hash = {'code' => code_element['code']}
+            if code_system
+              code_hash['code_system'] = code_system
+            end
+          end
+          code_hash
         end
 
       end
